7ab2bdd1206c2b52d057576ba34900f884fa3d94
arshadbadsha01 <arshad.badsha01@gmail.com>
2025-12-03 21:49:36 +0530

    4 modules add in app.http

--- server.js
+++ server.js
@@ -2,35 +2,34 @@ import express from 'express';
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-
-// Import your routes
 import postsRoutes from "./routes/posts.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 dotenv.config();
-
 const app = express();
 
-// Middlewares
 app.use(cors());
+
 app.use(express.json());
 
-// Routes
 app.use(postsRoutes);
 app.use(userRoutes);
 
-// Start Server Function
+
+
 const start = async () => {
 //   try {
-    await mongoose.connect("mongodb+srv://arshad:arshad123@cluster0.vv9bbwx.mongodb.net/?appName=Cluster0");   
+    const connectDB = await mongoose.connect("mongodb+srv://arshad:arshad123@cluster0.vv9bbwx.mongodb.net/?appName=Cluster0");
+
+    // console.log("✅ MongoDB is Connected Successfully");
+
     app.listen(9080, () => {
       console.log("🚀 Server is running on port 9080");
     });
-
 //   } catch (error) {
-//     console.error("❌ MongoDB Connection Error:", error.message);
+    // console.error("❌ MongoDB Connection Error:", error.message);
 //   }
-};
 
-// Call the function
+}
+
 start();
